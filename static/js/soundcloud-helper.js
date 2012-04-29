b05d8c9d569efd2a2f5525f0effe6d3c3a3c89a1
feeschmidts <fee.schmidtsoltau@gmail.com>
2012-04-29 12:50:07 +0100

    Working with one tile :)

--- static/js/soundcloud-helper.js
+++ static/js/soundcloud-helper.js
@@ -16,15 +16,17 @@
 	//Add soundcloud object
 	F.helpers.soundcloud = {
 		tpl: '<div id="fancybox-soundclip"><object height="81" width="100%">'
-			+'<param name="movie" value="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F43814758&amp;show_comments=true&amp;auto_play=false&amp;color=ff7700"></param>'
+			+'<param name="movie" value="SOUNDCLIP_URL"></param>'
 			+'<param name="allowscriptaccess" value="always"></param>'
-			+'<embed allowscriptaccess="always" height="81" src="https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F43814758&amp;show_comments=true&amp;auto_play=false&amp;color=ff7700" type="application/x-shockwave-flash" width="100"></embed>'
+			+'<embed allowscriptaccess="always" height="81" src="SOUNDCLIP_URL" type="application/x-shockwave-flash" width="100"></embed>'
 			+'</object></div>',
 		list: null,
 				
 		afterShow: function (opts) {
 			if (!this.list) {
-				this.list = $(opts.tpl || this.tpl).addClass(opts.position || 'top').appendTo('body');
+				var tpl = opts.tpl || this.tpl;
+				var html = tpl.replace("SOUNDCLIP_URL", opts.soundclip);
+				this.list = $(html).addClass(opts.position || 'top').appendTo('body');
 			}
 		},
 				
