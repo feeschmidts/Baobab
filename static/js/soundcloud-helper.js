6ced9e5320af4c03531d19e72ad4772bf6e0da37
Fee Schmidt-Soltau <fee.schmidtsoltau@gmail.com>
2012-05-30 17:46:22 +0100

    Soundcloud player changed to stratus

--- static/js/soundcloud-helper.js
+++ static/js/soundcloud-helper.js
@@ -15,27 +15,21 @@
 
 	//Add soundcloud object
 	F.helpers.soundcloud = {
-		tpl: '<div id="fancybox-soundclip"><object height="81" width="100%">'
-			+'<param name="movie" value="https://player.soundcloud.com/player.swf?url=SOUNDCLIP_URL&amp;show_comments=true&amp;auto_play=true&amp;color=ff7700"></param>'
-			+'<param name="allowscriptaccess" value="always"></param>'
-			+'<embed allowscriptaccess="always" height="81" src="https://player.soundcloud.com/player.swf?url=SOUNDCLIP_URL&amp;show_comments=true&amp;auto_play=true&amp;color=ff7700" type="application/x-shockwave-flash" width="100"></embed>'
-			+'</object></div>',
-		list: null,
-				
+	
 		afterShow: function (opts) {
-			if (!this.list) {
-				var tpl = opts.tpl || this.tpl;
-				var html = tpl.replace("SOUNDCLIP_URL", opts.soundclip);
-				this.list = $(html).addClass(opts.position || 'top').appendTo('body');
-			}
+			if (!$('#stratus').length) {
+				$.stratus({
+					auto_play: true,
+	      			download: false,
+	      			links: opts.soundclip
+	      		});
+	      	}
 		},
 				
 		beforeClose: function () {
-			if (this.list) {
-				this.list.remove();
+			if ($('#stratus').length) {
+				$('#stratus').remove();
 			}
-
-			this.list = null;
 		}
 	
 	}
